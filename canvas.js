b1267a17a156df5dbedd13cf49b3bdc61187275f
Monique <romaomonique4@gmail.com>
2022-02-07 00:06:54 -0300

    Correção na função desenhaTraco => canvas.js

--- canvas.js
+++ canvas.js
@@ -6,8 +6,8 @@ ctx.lineWidth = 2;
 
 function desenhaTraco(x1, x2) {
     ctx.beginPath(); //incrementar + 100
-    ctx.moveTo(x, 750); // x1=100
-    ctx.lineTo(x, 750); // x2=160
+    ctx.moveTo(x1, 750); // x1=100
+    ctx.lineTo(x2, 750); // x2=160
     ctx.stroke();
     ctx.closePath();
 }
