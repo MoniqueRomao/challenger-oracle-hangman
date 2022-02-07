71a05a387868b55a2969d31512e8843b887f7573
Monique <romaomonique4@gmail.com>
2022-02-06 21:32:50 -0300

    Array de palavras, função aleatoridade e verificação de repetição de palavras criada

---
+++ palavraSecreta.js
@@ -0,0 +1,27 @@
+var palavras = ["AXIOMA", "AZULEJO", "AMPULHETA", "BLITZ", "BACALHAU",
+"BOTIJA", "CRESPO", "CRIPTA", "CANDELABRO", "DUPLEX", "DESFIBRILADOR",
+"FANTOCHE", "FREEZER", "GRALHA", "GIRAR", "GNOSTICO", "INDIGNO", "INTRIGANTE",
+"MARFIM", "NOVELO", "OXIMETRO", "PSIQUE", "QUARTZO", "QUEPE", "QUIZ",
+"TONTO", "VALSA", "VAPORIZAR", "VERTIGINOSO", "XADREZ", "XILOFONE"];
+
+var usadas = [];
+
+
+function definePalavra () {
+
+    var indice = verificaIndice();
+
+    var palavra = palavras[indice];
+
+    return palavra;
+
+}
+function verificaIndice() {
+    var i = Math.round(Math.random()*(palavras.length - 1));
+    if (!usadas.includes(i)){
+        usadas.push(i);
+        return i;
+    } else {
+        verificaIndice();
+    }
+}
