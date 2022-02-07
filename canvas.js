cdae6984d02e4700fa0530b8c68c2db7fe02beb2
Monique <romaomonique4@gmail.com>
2022-02-07 18:47:07 -0300

    Funções letraCorreta e letraIncorreta criadas => canvas.js

--- canvas.js
+++ canvas.js
@@ -1,14 +1,23 @@
 var quadro = document.querySelector("#forca");
-var btnInicia = document.querySelector("#iniciar-jogo");
 var ctx = quadro.getContext("2d");
 ctx.lineWidth = 2;
 
 
-function desenhaTraco(x1, x2) {
-    ctx.beginPath(); //incrementar + 100
+function desenhaLinha(x1, x2) {
+    //incrementar + 100
     ctx.moveTo(x1, 750); // x1=100
     ctx.lineTo(x2, 750); // x2=160
     ctx.stroke();
-    ctx.closePath();
 }
 
+function letraCorreta(l, x1) {
+    //x1 = (100+15)*indice da letra
+    ctx.font = "50px Verdana";
+    ctx.fillText(l, x1, 750, 50);
+}
+function letraIncorreta(l, erros){
+    x = 300 + (100 * erros);
+    ctx.font = "50px Verdana";
+    ctx.fillStyle = "red";
+    ctx.fillText(l, x, 400, 50);
+}
